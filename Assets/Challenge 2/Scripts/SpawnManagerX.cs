8739d0885e591ac0df5037084abc16f3c7b4ff86
StarCodes2 <55100341+StarCodes2@users.noreply.github.com>
2022-09-01 18:45:03 +0100

    Fixing Bugs

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,13 +10,13 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float startDelay = 3.0f;
+    private float spawnInterval;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", startDelay);
     }
 
     // Spawn random ball at random x position at top of play area
@@ -29,6 +29,9 @@ public class SpawnManagerX : MonoBehaviour
 
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+        spawnInterval = Random.Range(3, 5);
+        Invoke("SpawnRandomBall", spawnInterval);
     }
 
 }
